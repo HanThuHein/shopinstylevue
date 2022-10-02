30fb4ed13f9de07ad203caaccc34af64f0d3352b
hanthuhein <hanthu620@gmail.com>
2022-10-02 21:40:02 +0630

    link

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header title='About Page' subtitle = "say something"/>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+export default{
+  name:"AboutView",
+  components:{Header}
+}
+
+</script>
